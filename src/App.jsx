2e4f7e77422092ecb7ca0fe147e09f8b817a20ae
amarildoexe <amarildothedesigner@gmail.com>
2025-03-29 20:24:09 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,162 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+
+<section className="bg-blue-700">
+  <div className="flex flex-col items-center justify-center">
+    <div className="">
+      <h1 className="mx-4 my-4 text-start sm:text-center text-6xl font-bold text-white sm:text-7xl md:text-8xl lg:text-9xl">Find that<br />Exclusive<br />Unicorn.</h1>
+      <p className="mx-4 my-4 text-start sm:text-center text-sm text-white sm:text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sunt quam laudantium, nemo obcaecati beatae, voluptatibus quisquam cumque deserunt deleniti incidunt, ea magnam quod aperiam nihil maiores officia natus fugit rem adipisci perferendis dolor? Possimus eos iusto, ipsum, eveniet voluptatum voluptas maxime nisi aliquam earum rem cum, quos esse amet rerum recusandae optio similique quod!</p>
+      <div className="flex flex-row items-center justify-center">
+        <button className="mx-4 rounded-4xl my-4 bg-blue-400 px-10 py-1 font-bold text-white sm:px-25 sm:py-4 hover:bg-blue-800">ENROLL</button>
+        <button className="mx-4 my-4 rounded-4xl border-3 px-10 py-1 text-white sm:px-25 sm:py-4">GET</button>
+      </div>
+
+      <h1 className="mx-4 my-4 text-start text-6xl font-bold text-white sm:text-center">Let's get <br className="block sm:hidden" />Into it Right On<br className="block sm:hidden" />Point.</h1>
+    </div>
+  </div>
+</section>
+
+<section className="bg-white">
+  <h1 className="mx-4 my-4 text-start text-6xl font-bold text-blue-700 sm:text-center">What i can <br className="block sm:hidden" />Turn Into Right <br className="block sm:hidden" />Point.</h1>
+
+  <div className="flex flex-col items-center justify-center sm:flex-row flex-wrap gap-8">
+    
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Figma</h1>
+      <p className="text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4 m-0.5" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Photoshop</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-20 w-20 mt-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrN56wUMxUYEH-Q2K2ozuAwyJbY-FX6NcBpw&s" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Premiere Pro</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-25 w-25 mt-4" src="https://logowik.com/content/uploads/images/adobe-premiere-20209132.jpg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Filmora</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4" src="https://freelogopng.com/images/all_img/1682566315logo-filmora.png" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Figma</h1>
+      <p className="text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4 m-0.5" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Photoshop</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-20 w-20 mt-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrN56wUMxUYEH-Q2K2ozuAwyJbY-FX6NcBpw&s" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Premiere Pro</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-25 w-25 mt-4" src="https://logowik.com/content/uploads/images/adobe-premiere-20209132.jpg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Filmora</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4" src="https://freelogopng.com/images/all_img/1682566315logo-filmora.png" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Figma</h1>
+      <p className="text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4 m-0.5" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Photoshop</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-20 w-20 mt-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrN56wUMxUYEH-Q2K2ozuAwyJbY-FX6NcBpw&s" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Premiere Pro</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-25 w-25 mt-4" src="https://logowik.com/content/uploads/images/adobe-premiere-20209132.jpg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Filmora</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4" src="https://freelogopng.com/images/all_img/1682566315logo-filmora.png" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Figma</h1>
+      <p className="text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4 m-0.5" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Photoshop</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-20 w-20 mt-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrN56wUMxUYEH-Q2K2ozuAwyJbY-FX6NcBpw&s" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Premiere Pro</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-25 w-25 mt-4" src="https://logowik.com/content/uploads/images/adobe-premiere-20209132.jpg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Filmora</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4" src="https://freelogopng.com/images/all_img/1682566315logo-filmora.png" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Figma</h1>
+      <p className="text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4 m-0.5" src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Photoshop</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-20 w-20 mt-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrN56wUMxUYEH-Q2K2ozuAwyJbY-FX6NcBpw&s" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Premiere Pro</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-25 w-25 mt-4" src="https://logowik.com/content/uploads/images/adobe-premiere-20209132.jpg" alt=""/>
+    </div>
+
+    <div className="w-60 h-60 mx-4 my-4 p-10 hover:shadow-2xl flex flex-col items-center justify-center">
+      <h1 className="text-center font-bold text-2xl">Filmora</h1>
+      <p className="text-center text-sm m-0.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam.</p>
+      <img className="h-15 w-15 mt-4" src="https://freelogopng.com/images/all_img/1682566315logo-filmora.png" alt=""/>
+    </div>
+
+
+
+
+
+
+
+  </div>
+</section>
+
+
+
+    </>
+  )
+}
+
+export default App
